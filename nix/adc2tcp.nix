7d50391af0dc2418b9852e7212d6a0b952bcb01e
Astro <astro@spaceboyz.net>
2019-03-18 23:09:54 +0100

    nix: build with fetchcargo

--- nix/adc2tcp.nix
+++ nix/adc2tcp.nix
@@ -1,13 +1,45 @@
-{ stdenv, rustPlatform }:
+{ stdenv, rustPlatform, cacert, git, cargo-vendor }:
 
 with rustPlatform;
+let
+  fetchcargo = import ./fetchcargo.nix {
+    inherit stdenv cacert git cargo-vendor;
+    inherit (rust) cargo;
+  };
+  adc2tcpDeps = fetchcargo {
+    name = "adc2tcp-deps";
+    src = ../.;
+    sha256 = "1wddmsdsqwfzld43g12l2r60ndfwi8ihwm03zzcs9722sirw49fz";
+  };
+in
 
 buildRustPackage rec {
   name = "adc2tcp";
   version = "0.0.0";
 
   src = ../.;
-  cargoSha256 = "0q3cn3jzgmrqiymisxymn19vbnnpsj7if052w5zh25x9ikin6lpl";
+  cargoSha256 = "1wddmsdsqwfzld43g12l2r60ndfwi8ihwm03zzcs9722sirw49fz";
+
+  buildInputs = [ adc2tcpDeps ];
+  patchPhase = ''
+    cat >> .cargo/config <<EOF
+    [source.crates-io]
+    replace-with = "vendored-sources"
+
+    [source.vendored-sources]
+    directory = "${adc2tcpDeps}"
+
+    [source."https://github.com/stm32-rs/stm32f4xx-hal"]
+    git = "https://github.com/stm32-rs/stm32f4xx-hal"
+    branch = "master"
+    replace-with = "vendored-sources"
+
+    [source."https://github.com/stm32-rs/stm32-eth"]
+    git = "https://github.com/stm32-rs/stm32-eth"
+    branch = "master"
+    replace-with = "vendored-sources"
+    EOF
+  '';
 
   doCheck = false;
   installPhase = ''
